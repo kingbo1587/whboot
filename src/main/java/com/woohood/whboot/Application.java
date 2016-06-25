f6ec3dfcdc967ea8fe5464e591c57abfb08ef8e5
kingbo1587 <kingbo1587@163.com>
2016-06-25 17:41:02 +0800

    WB-000: add spring boot tools

--- src/main/java/com/woohood/whboot/Application.java
+++ src/main/java/com/woohood/whboot/Application.java
@@ -2,19 +2,13 @@ package com.woohood.whboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@Controller
+@Configuration
 @EnableAutoConfiguration
+@ComponentScan
 public class Application {
-
-	@RequestMapping("/")
-	@ResponseBody
-	String home() {
-		return "Hello World2!";
-	}
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
