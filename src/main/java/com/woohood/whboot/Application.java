67381281a75e77f6e05f42804d87aad0b3790432
kingbo1587 <kingbo1587@163.com>
2016-06-27 07:53:37 +0800

    WB-000: add mvc part config

--- src/main/java/com/woohood/whboot/Application.java
+++ src/main/java/com/woohood/whboot/Application.java
@@ -2,16 +2,38 @@ package com.woohood.whboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
+@PropertySources({ @PropertySource("classpath:config/config.properties") })
 public class Application {
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
+	}
+
+	/**
+	 * 解决post中文乱码
+	 * 
+	 * @return
+	 */
+	@Bean
+	public FilterRegistrationBean characterEncodingFilter() {
+		final CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		final FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+		filterRegistrationBean.setFilter(characterEncodingFilter);
+		filterRegistrationBean.addUrlPatterns("/*");
+		return filterRegistrationBean;
 	}
 
 }
--- src/main/java/com/woohood/whboot/web/HelloController.java
+++ src/main/java/com/woohood/whboot/web/HelloController.java
@@ -1,5 +1,8 @@
 package com.woohood.whboot.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,11 +10,24 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 	
+	private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+	
+	@Value("${hello.rivulet}")
+	private String msg;
+
 	@RequestMapping("/")
 	@ResponseBody
 	String home() {
-		return "Hello World222!";
+		return "Hello World222!" + msg;
+	}
+	
+	@RequestMapping("first")
+	String first(){
+		return "/WEB-INF/resources/templates/first.html";
+	}
+	
+	public static void main(String[] args) {
+		logger.info("Hello World!!!! logger");
 	}
 	
 }
-
